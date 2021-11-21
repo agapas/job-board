025f298bec20dcf0c50ce825ffdfdeaa0ec000c5
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-11-21 15:41:22 +0000

    Added graphqlRequest common function with basic error handling

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,45 +1,48 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
-export async function loadJobs() {
+async function graphqlRequest(query, variables={}) {
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({
-      query: `{
-        jobs {
-          id
-          title
-            company {
-              id
-              name
-            }
-        }
-      }`
-    })
+    body: JSON.stringify({ query, variables }),
   });
   const responseBody = await response.json();
-  return responseBody.data.jobs;
+
+  if (responseBody.errors) {
+    const message = responseBody.errors.map(error => error.message).join('\n');
+    throw new Error(message);
+  }
+
+  return responseBody.data;
+}
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title
+        company {
+          id
+          name
+        }
+    }
+  }`;
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
 }
 
 export async function loadJob(id) {
-  const response = await fetch(endpointURL, {
-    method: 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({
-      query: `query JobQuery($id: ID!) {
-        job(id: $id) {
-          id
-          title
-          description
-          company {
-            id
-            name
-          }
-        }
-      }`,
-      variables: { id }
-    })
-  });
-  const responseBody = await response.json();
-  return responseBody.data.job;
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      description
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const { job } = await graphqlRequest(query, { id });
+  return job;
 }
