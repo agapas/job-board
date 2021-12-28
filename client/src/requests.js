fc5f310ef9f4b1a8c8066081c07dc8b9f10b732d
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-12-28 14:24:16 +0000

    Used no-cache fetchPolicy in loadJobs request

--- client/src/requests.js
+++ client/src/requests.js
@@ -50,7 +50,7 @@ export async function loadJobs() {
         }
     }
   }`;
-  const { data: { jobs } } = await client.query({ query });
+  const { data: { jobs } } = await client.query({ query, fetchPolicy: 'no-cache' });
   return jobs;
 }
 
